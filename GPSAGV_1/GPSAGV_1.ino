18ed73733f6043020cc91c33f341355ad5019993
unknown <josh@SPARE-WS.tesseract.local>
2014-10-30 23:26:13 -0500

    Added LCD code

--- GPSAGV_1/GPSAGV_1.ino
+++ GPSAGV_1/GPSAGV_1.ino
@@ -1,5 +1,5 @@
 //compiled on 1.5.1r2
-
+#include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>  
 #include <Wire.h>
@@ -15,6 +15,18 @@ float heading=0;
 int headinggps;
 TinyGPS gps;
 SoftwareSerial mySerial(2, 3);    //used for gps rx and tx pins in use
+
+//LCD stuff
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); 
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
 
 int buttonreaddone;
 
@@ -40,6 +52,10 @@ void printFloat(double f, int digits = 2);
 
 
   void setup(){
+    lcd.begin(16, 2);              // start the library
+    lcd.setCursor(0,0);
+    lcd.print("NEW TESTING..."); // print a simple message
+ 
     //Setup compass
     slaveAddress = HMC6352Address >> 1;  
     Wire.begin();
@@ -348,6 +364,20 @@ void printFloat(double f, int digits = 2);
     leftturn();
   }
 
+  // read the buttons
+  int read_LCD_buttons()
+  {
+   adc_key_in = analogRead(0);      // read the value from the sensor
+   // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+   // we add approx 50 to those values and check to see if we are close
+   if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+   if (adc_key_in < 50)   return btnRIGHT; 
+   if (adc_key_in < 195)  return btnUP;
+   if (adc_key_in < 380)  return btnDOWN;
+   if (adc_key_in < 555)  return btnLEFT;
+   if (adc_key_in < 790)  return btnSELECT;  
+   return btnNONE;  // when all others fail, return this...
+  }
 
   void buttonread(){
   
