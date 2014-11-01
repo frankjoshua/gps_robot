2f5a24c7290f78006684ba6d944cfabeeb8ba2bd
frankjoshua <frankjoshua@gmail.com>
2014-10-31 21:08:49 -0500

    Corrected distance and cleaned code

--- GPSAGV_1/GPSAGV_1.ino
+++ GPSAGV_1/GPSAGV_1.ino
@@ -6,15 +6,9 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
-//Compass stuff
-int HMC6352Address = 0x42;
-int slaveAddress;
-byte headingData[2];
-int i, headingValue;
 // tiny gps library code
 float flat, flon;
-float heading=0;
-int headinggps;
+
 TinyGPS gps;
 SoftwareSerial mySerial(2, 3);    //used for gps rx and tx pins in use
 
@@ -33,28 +27,26 @@ int adc_key_in  = 0;
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
+
 //Menu System
+#define CMD_NAVIGATE -1
+#define CMD_SHOW_POSITION 0
+#define CMD_ADD_WAY_POINT 1
+int mMenuPosition = CMD_SHOW_POSITION;
 #define MENU_LENGTH 4
-#define CMD_DIRECTION 0
-#define CMD_SHOW_POSITION 1
-#define CMD_ADD_WAY_POINT 2
-char* mMenu[]={"Direction", "Show Position", "Add Way Point", "Delete Way Point",
+char* mMenu[]={
+"Show Position",
+"Add Way Point",
+"Delete Way Point",
 "Clear Way Points"};
-int mMenuPosition = 0;
-
-#define led 13
-int x4=0;
-#define buttons 0
-
-//Current position
-float x2lat;
-float x2lon;
+#define MODE_MENU 0
+#define MODE_NAVIGATE 1
+#define NUMBER_OF_MODES 2
+int mMode = MODE_MENU;
 
 //Store Waypoints
-int waypoints;   // number of waypoints
-int waycont=1; //Current waypoint
 #define MAX_WAYPOINTS 20
-float mLat[MAX_WAYPOINTS]; //28.324603,-82.267419 home testing
+float mLat[MAX_WAYPOINTS]; //38.629983, -90.596550
 float mLon[MAX_WAYPOINTS];
 
 //Method definitions
@@ -66,20 +58,15 @@ void printFloat(double f, int digits = 2);
   void setup(){
     lcd.begin(16, 2);              // start the library
     lcd.setCursor(0,0);
-    lcd.print("NEW TESTING..."); // print a simple message
+    lcd.print("Starting..."); // print a simple message
  
-    //Setup compass
-    slaveAddress = HMC6352Address >> 1;  
-    Wire.begin();
     //Set up diagnostic output
     Serial.begin(115200);
-    Serial.println("Booting ...");
+    Serial.println("Starting ...");
+    
     //Set gps
     mySerial.begin(4800);
-    //Setup output led
-    pinMode(led, OUTPUT);
-    //Setup input buttons
-    pinMode(buttons, INPUT);
+
     /* Initialise the compass */
     if(!mag.begin()){
       /* There was a problem detecting the HMC5883 ... check your connections */
@@ -87,8 +74,9 @@ void printFloat(double f, int digits = 2);
       while(1);
     }
     //Setup test waypoint
-    mLon[0] = -82.267419;
-    mLat[1] = 28.324603;
+    mLat[0] = 38.629983;
+    mLon[0] = -90.596550;
+    
     //Show initial menu
     displayMenu();
   }
@@ -152,8 +140,22 @@ void printFloat(double f, int digits = 2);
           mMenuPosition++;
         }
       break;
+      case btnRIGHT:
+        //Next Mode
+        if(mMode < NUMBER_OF_MODES - 1){
+           mMode++; 
+        }
+      break;
+      case btnLEFT:
+        //Last Mode
+        if(mMode > 0){
+           mMode--; 
+        }
+      break;
       case btnSELECT:
-        executeCommand(mMenuPosition);
+        if(mMode == MODE_MENU){
+          executeCommand(mMenuPosition);
+        }
       break;
       default:
         //No buttons pressed
@@ -162,9 +164,17 @@ void printFloat(double f, int digits = 2);
     }
     
     //Diplay menu if changed
-    if(isPress){
-      displayMenu();
+    switch(mMode){
+       case MODE_MENU:
+        if(isPress){
+          displayMenu();
+        }
+       break;
+       case MODE_NAVIGATE:
+         executeCommand(CMD_NAVIGATE);
+       break;
     }
+    
     //Short delay to prevent rebounce
     delay(150);
   }
@@ -188,22 +198,27 @@ void printFloat(double f, int digits = 2);
          lcd.setCursor(0,0);
          lcd.print("LAT: ");
          lcd.print(flat);
-         lcd.print(" ");
-         lcd.print(compassHeading);
          lcd.setCursor(0,1);
          lcd.print("LON: ");
          lcd.print(flon);
-         lcd.print(" ");
-         lcd.print(getDistance(0, flat, flon));
-         delay(500);
+         delay(1000);
        break;
-       case CMD_DIRECTION:
-         
+       case CMD_NAVIGATE:
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print("MAG:");
+         lcd.print(compassHeading);
+         lcd.setCursor(0,1);
+         lcd.print("GPS:");
+         lcd.print(getHeading(0, flat, flon));
+         lcd.print(" ");
+         lcd.print("M:");
+         lcd.print(getDistance(0, flat, flon));
+         delay(100);
        break;
     }
     
   }
-  
  
   void gpsdump(TinyGPS &gps){
     unsigned long age, date, time, chars;
@@ -237,15 +252,18 @@ void printFloat(double f, int digits = 2);
   * Pass in the current Latitude and Longitude
   */
   int getDistance(int wayPoint, float currentLat, float currentLon){
-    //Convert everthing to radians
-    float waypointLat = radians(mLat[wayPoint]);
-    float waypointLon = radians(mLon[wayPoint]);
-    currentLat = radians(currentLat);
-    currentLon = radians(currentLon);
+    //Convert to radians
+    float waypointLat = mLat[wayPoint];
+    float waypointLon = mLon[wayPoint];
+    
+    //currentLon = radians(currentLon);
     
     //Find the differance of lat an lon
     float diffLat = waypointLat - currentLat;
-    float diffLon = waypointLon - currentLon;
+    currentLat = radians(currentLat);
+    waypointLat = radians(waypointLat);
+    float diffLon = radians(waypointLon - currentLon);
+    
     
     //Calculate the distance
     float distance = (sin(diffLat/2.0)*sin(diffLat/2.0));
@@ -254,7 +272,7 @@ void printFloat(double f, int digits = 2);
     distance2 *= sin(diffLon/2.0);
     distance2 *= sin(diffLon/2.0);
     distance += distance2;
-    distance = (2*atan2(sqrt(distance),sqrt(1.0-distance)));
+    distance = 2 * atan2(sqrt(distance), sqrt(1.0-distance));
     
     //Convert to meters
     distance *= 6371000.0;
@@ -262,209 +280,27 @@ void printFloat(double f, int digits = 2);
     return distance;
   }
   
-//------------------------------below is the part of the code where everything is calculated
-  void distance(){
-    x2lat = mLat[waycont - 1]; 
-    x2lon = mLon[waycont - 1];
-   
-    float flat1=flat;            
-    float flon1=flon;
-    float dist_calc=0;
-    float dist_calc2=0;
-    float diflat=0;
-    float diflon=0;
+  /**
+  * Returns heading to a waypoint in degrees
+  * Pass in the wayPoint and current lattitude and longitude
+  */
+  float getHeading(int wayPoint, float currentLat, float currentLon){
+    //Convert everthing to radians
+    float waypointLat = radians(mLat[wayPoint]);
+    float waypointLon = radians(mLon[wayPoint]);
+    currentLat = radians(currentLat);
+    currentLon = radians(currentLon);
     
-    //------------------------------------------ distance formula below. Calculates distance from current location to waypoint
-    diflat=radians(x2lat-flat1);  //notice it must be done in radians
-    flat1=radians(flat1);
-    x2lat=radians(x2lat);
-    diflon=radians((x2lon)-(flon1));
-    
-    dist_calc = (sin(diflat/2.0)*sin(diflat/2.0));
-    dist_calc2= cos(flat1);
-    dist_calc2*=cos(x2lat);
-    dist_calc2*=sin(diflon/2.0);                                       
-    dist_calc2*=sin(diflon/2.0);
-    dist_calc +=dist_calc2;
-    
-    dist_calc=(2*atan2(sqrt(dist_calc),sqrt(1.0-dist_calc)));
-    
-    dist_calc*=6371000.0; //Converting to meters
-    Serial.println("distance");
-    Serial.println(dist_calc);
-    if(dist_calc<2){
-      if(waycont==waypoints){
-        done();
-      }
-      waycont+=1;
-    }
-    
-    //-----------------------------------------heading formula below. Calculates heading to the waypoint from the current locaiton
-    flon1 = radians(flon1);  //also must be done in radians
-    x2lon = radians(x2lon);
-    
-    heading = atan2(sin(x2lon-flon1)*cos(x2lat),cos(flat1)*sin(x2lat)-sin(flat1)*cos(x2lat)*cos(x2lon-flon1)),2*3.1415926535;
+    double arg1 = sin(waypointLon-currentLon)*cos(waypointLat);
+    double arg2 = cos(currentLat)*sin(waypointLat)-sin(currentLat)*cos(waypointLat)*cos(waypointLon-currentLon);
+    float heading = atan2(arg1, arg2);//,2*3.1415926535;
     heading = heading*180/3.1415926535;  // convert from radians to degrees
-    int head =heading; 
-    if(head<0){
-      heading+=360;   //if the heading is negative then add 360 to make it positive
+    //if the heading is negative then add 360 to make it positive
+    if(heading < 0){
+      heading+=360;   
     }
     
-    Serial.println("heading:");
-    Serial.println(heading);
-    //-------------------------------------------------------------
-    
-    int turn=0;
-    Wire.beginTransmission(slaveAddress);        //the wire stuff is for the compass module
-    Wire.write("A");              
-    Wire.endTransmission();
-    delay(10);                  
-    Wire.requestFrom(slaveAddress, 2);       
-    i = 0;
-    while(Wire.available() && i < 2){ 
-      headingData[i] = Wire.read();
-      i++;
-    }
-    headingValue = headingData[0]*256 + headingData[1];
-    int pracheading = headingValue / 10;      // this is the heading of the compass
-    if(pracheading>0){
-       headinggps=pracheading;
-    }
-    Serial.println("current heaDING:");
-    Serial.println(headinggps);
-    x4=headinggps-heading;   //getting the difference of our current heading to our needed heading
-    
-    //Serial.println(absolute);
-    int x5;
-    //-------------------------------------- below tells us which way we need to turn
-    if(x4>=-180){
-      if(x4<=0){
-        turn=8;    //set turn =8 which means "right"         
-      }
-    }
-    if(x4<-180){
-      turn=5;      //set turn = 5 which means "left"
-    }
-    if(x4>=0){
-      if(x4<180){
-        turn=5;   //set turn = 5 which means "left"
-      }
-    }
-    if(x4>=180){     //set turn =8 which means "right"
-      turn=8;
-    }
-    //-----------------------------------------------------------------------------------------------------------------------------------
-    //-----------------------------------------------------------------------------------------------------------------------------------
-    //-------------------------------------------------------------real important turning stuff. DO NOT TOUCH!!!
-    float hd = headinggps;
-    if(hd==heading){
-        turn=3;   //then set turn = 3 meaning go "straight" 
-    }
-    
-    if(turn==3){   
-      Serial.println("straight");
-      //digitalWrite(mo1, LOW);               //go straight 
-      //digitalWrite(mo2, HIGH);
-      //digitalWrite(mo3, LOW);
-      //digitalWrite(mo4, LOW);
-    }
-    //-------------------------------------------------------------------------------------turn right
-    if(turn==8){
-       rightturn();
-    }
-    //------------------------------------------------------------------------------------------turn left
-    if(turn==5){
-       leftturn();
-    }
-     //-------------------------------------------------------------------------
-  }
-
-  
-
-  void done(){
-    //Stop Motors Here
-  }
-//----------------------------------------------------------------------------------
-//---------------------------------------------------------------------------
-//------------------------------------------------------right turning
-  void rightturn(){
-    if(headinggps+2>heading){
-      if(headinggps-2<heading){
-//        digitalWrite(mo3, LOW);
-//        digitalWrite(mo4, LOW);
-        return;
-      }
-    }
-          x4=headinggps-heading;  
-    if(x4<-180){
-    return;
-    }
-    if(x4>=0){
-      if(x4<180){
-      return;
-      }
-    }
-    
-//      digitalWrite(mo1, LOW);
-//      digitalWrite(mo2, HIGH);   
-//      digitalWrite(mo3, HIGH);
-//      digitalWrite(mo4, LOW); 
-      Wire.beginTransmission(slaveAddress);        //the wire stuff is for the compass module
-      Wire.write("A");              
-      Wire.endTransmission();
-      delay(10);                  
-      Wire.requestFrom(slaveAddress, 2);       
-      i = 0;
-      while(Wire.available() && i < 2)
-      { 
-        headingData[i] = Wire.read();
-        i++;
-      }
-      headingValue = headingData[0]*256 + headingData[1];  
-    headinggps = headingValue / 10;      // this is the heading of the compass
-    rightturn();
-  }
-
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------
-//----------------------------------------------left turning
-  void leftturn(){
-    if(headinggps+2>heading){
-      if(headinggps-2<heading){
-//         digitalWrite(mo3, LOW);
-//        digitalWrite(mo4, LOW);
-        return;
-      }
-    }
-    x4=headinggps-heading;  
-    if(x4>=-180){
-      if(x4<=0){
-         return;         
-      }
-    }
-  
-    if(x4>=180){     
-      return;
-    }
-  
-//    digitalWrite(mo1, LOW);
-//    digitalWrite(mo2, HIGH);
-//    digitalWrite(mo3, LOW);
-//    digitalWrite(mo4, HIGH); 
-    Wire.beginTransmission(slaveAddress);        //the wire stuff is for the compass module
-    Wire.write("A");              
-    Wire.endTransmission();
-    delay(10);                  
-    Wire.requestFrom(slaveAddress, 2);       
-    i = 0;
-    while(Wire.available() && i < 2)
-    { 
-      headingData[i] = Wire.read();
-      i++;
-    }
-    headingValue = headingData[0]*256 + headingData[1];  
-    headinggps = headingValue / 10;      // this is the heading of the compass
-    leftturn();
+    return heading;
   }
 
   // read the buttons
