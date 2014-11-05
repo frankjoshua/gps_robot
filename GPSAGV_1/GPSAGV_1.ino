e72b74f2b4d893e5f6ce02036a7b7e4d8d6c6b3a
unknown <josh@SPARE-WS.tesseract.local>
2014-11-04 22:31:52 -0600

    Added new mode and better diastance calc

--- GPSAGV_1/GPSAGV_1.ino
+++ GPSAGV_1/GPSAGV_1.ino
@@ -29,23 +29,25 @@ int adc_key_in  = 0;
 #define btnNONE   5
 
 //Menu System
+#define CMD_SHOW_POSITION -2
 #define CMD_NAVIGATE -1
-#define CMD_SHOW_POSITION 0
+#define CMD_SHOW_WAY_POINT 0
 #define CMD_ADD_WAY_POINT 1
 #define CMD_NEXT_WAY_POINT 2
 #define CMD_LAST_WAY_POINT 3
-int mMenuPosition = CMD_SHOW_POSITION;
+int mMenuPosition = CMD_SHOW_WAY_POINT;
 #define MENU_LENGTH 4
 char* mMenu[]={
-"Show Position",
+"Show Way Point",
 "Add Way Point",
 "Next Way Point",
 "Last Way Point",
 "Delete Way Point",
 "Clear Way Points"};
-#define MODE_MENU 0
-#define MODE_NAVIGATE 1
-#define NUMBER_OF_MODES 2
+#define MODE_POSITION 0
+#define MODE_MENU 1
+#define MODE_NAVIGATE 2
+#define NUMBER_OF_MODES 3
 int mMode = MODE_MENU;
 
 //Store Waypoints
@@ -87,13 +89,15 @@ void printFloat(double f, int digits = 2);
   }
 
   void loop(){ 
+    bool newData = false;
     //Read from gps
     if (feedgps()){
       //Dump gps if new data
       gpsdump(gps);
+      newData = true;
     }
     updateHeading();
-    updateDisplay();
+    updateDisplay(newData);
   }
 
   /**
@@ -127,7 +131,7 @@ void printFloat(double f, int digits = 2);
     compassHeading = heading * 180/M_PI; 
   }
   
-  void updateDisplay(){
+  void updateDisplay(bool newData){
     //Check if a button was pressed
     int btnPressed = read_LCD_buttons();
     
@@ -141,7 +145,7 @@ void printFloat(double f, int digits = 2);
       break; 
       case btnDOWN:
         //Move menu down
-        if(mMenuPosition < MENU_LENGTH - 1){
+        if(mMenuPosition < MENU_LENGTH){
           mMenuPosition++;
         }
       break;
@@ -176,12 +180,21 @@ void printFloat(double f, int digits = 2);
         }
        break;
        case MODE_NAVIGATE:
-         executeCommand(CMD_NAVIGATE);
+         if(isPress || newData){
+           executeCommand(CMD_NAVIGATE);
+         }
+       break;
+       case MODE_POSITION:
+         if(isPress || newData){
+           executeCommand(CMD_SHOW_POSITION);
+         }
        break;
     }
     
     //Short delay to prevent rebounce
-    delay(150);
+    if(isPress){
+      delay(250);
+    }
   }
 
   void displayMenu(){
@@ -203,24 +216,35 @@ void printFloat(double f, int digits = 2);
          lcd.clear();
          lcd.setCursor(0,0);
          lcd.print("LAT: ");
-         lcd.print(flat);
+         lcd.print(flat, 6);
          lcd.setCursor(0,1);
          lcd.print("LON: ");
-         lcd.print(flon);
-         delay(1000);
+         lcd.print(flon, 6);
+         //delay(1000);
+       break;
+       case CMD_SHOW_WAY_POINT:
+         //Display current position
+         lcd.clear();
+         lcd.setCursor(0,0);
+         lcd.print("LAT: ");
+         lcd.print(mLat[mCurrentWayPoint], 6);
+         lcd.setCursor(0,1);
+         lcd.print("LON: ");
+         lcd.print(mLon[mCurrentWayPoint], 6);
+         delay(2000);
        break;
        case CMD_NAVIGATE:
          lcd.clear();
          lcd.setCursor(0,0);
-         lcd.print("MAG:");
+         lcd.print("M:");
          lcd.print(compassHeading);
          lcd.setCursor(0,1);
-         lcd.print("GPS:");
+         lcd.print("G:");
          lcd.print(getHeading(mCurrentWayPoint, flat, flon));
          lcd.print(" ");
-         lcd.print("M:");
+         lcd.print("D:");
          lcd.print(getDistance(mCurrentWayPoint, flat, flon));
-         delay(100);
+         //delay(100);
        break;
        case CMD_ADD_WAY_POINT:
          mLat[mCurrentWayPoint] = flat;
@@ -273,31 +297,41 @@ void printFloat(double f, int digits = 2);
     //Convert to radians
     float waypointLat = mLat[wayPoint];
     float waypointLon = mLon[wayPoint];
-    
-    //currentLon = radians(currentLon);
-    
-    //Find the differance of lat an lon
-    float diffLat = waypointLat - currentLat;
-    currentLat = radians(currentLat);
-    waypointLat = radians(waypointLat);
-    float diffLon = radians(waypointLon - currentLon);
-    
-    
-    //Calculate the distance
-    float distance = (sin(diffLat/2.0)*sin(diffLat/2.0));
-    float distance2 = cos(currentLat);
-    distance2 *= cos(waypointLat);
-    distance2 *= sin(diffLon/2.0);
-    distance2 *= sin(diffLon/2.0);
-    distance += distance2;
-    distance = 2 * atan2(sqrt(distance), sqrt(1.0-distance));
-    
-    //Convert to meters
-    distance *= 6371000.0;
-    
-    return distance;
+      
+    return calc_dist(currentLat, currentLon, waypointLat, waypointLon);
   }
   
+  /*************************************************************************
+ * //Function to calculate the distance between two waypoints
+ *************************************************************************/
+float calc_dist(float flat1, float flon1, float flat2, float flon2)
+{
+  float dist_calc=0;
+  float dist_calc2=0;
+  float diflat=0;
+  float diflon=0;
+  
+  //I've to spplit all the calculation in several steps. If i try to do it in a single line the arduino will explode.
+  diflat=radians(flat2-flat1);
+  flat1=radians(flat1);
+  flat2=radians(flat2);
+  diflon=radians((flon2)-(flon1));
+  
+  dist_calc = (sin(diflat/2.0)*sin(diflat/2.0));
+  dist_calc2= cos(flat1);
+  dist_calc2*=cos(flat2);
+  dist_calc2*=sin(diflon/2.0);
+  dist_calc2*=sin(diflon/2.0);
+  dist_calc +=dist_calc2;
+  
+  dist_calc=(2*atan2(sqrt(dist_calc),sqrt(1.0-dist_calc)));
+  
+  dist_calc*=6371000.0; //Converting to meters
+  //Serial.println(dist_calc);
+  return dist_calc;
+}
+
+
   /**
   * Returns heading to a waypoint in degrees
   * Pass in the wayPoint and current lattitude and longitude
